8eb502aed036ec640646e4731ec2df72b7b2f16e
lg144 <42222880+lg144@users.noreply.github.com>
2021-02-28 18:38:09 -0500

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by PA2 Draft CAP.rc
+//
+#define IDC_MYICON                      2
+#define IDD_PA2DRAFTCAP_DIALOG          102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_PA2DRAFTCAP                 107
+#define IDI_SMALL                       108
+#define IDC_PA2DRAFTCAP                 109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     130
+#define IDI_ICON1                       131
+#define IDCANCEL                        1002
+#define IDXCANCEL                       1002
+#define IDC_EDIT1                       1003
+#define ID_FILE_OPENAWINDOW             32771
+#define ID_OPEN_ANEWWINDOW              32772
+#define ID_SETTINGS_FILLCOLOR           32773
+#define ID_SETTINGS_FILLPATTERN         32774
+#define ID_FILLPATTERN_FLY              32775
+#define ID_FILLPATTERN_HATCHED          32776
+#define ID_FILLPATTERN_HALFTONE         32777
+#define ID_SETTINGS_CONCAVEFILLCOLOR    32778
+#define ID_SETTINGS_BORDERCOLOR         32779
+#define ID_FILE_OPENAWINDOW32780        32780
+#define ID_FILLPATTERN_SOLID            32781
+#define ID_SETTINGS_BORDERPATTERN       32782
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32783
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
